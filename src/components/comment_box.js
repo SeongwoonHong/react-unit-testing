18a412f4787f85a468498c11c19bc4aa7bf922ed
seongwoon hong <saytoyou91@gmail.com>
2018-02-17 17:17:17 -0500

    Implement unit test for comment box

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      comment: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      comment: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      comment: ''
+    });
+  }
   render() {
     return (
-      <div className="comment-box">
-        <textarea />
-        <button>Submit Comment</button>
-      </div>
+      <form onSubmit={this.handleSubmit} className="comment-box">
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.comment}
+        />
+        <button type="submit">Submit Comment</button>
+      </form>
     );
   }
 }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -18,4 +18,18 @@ describe('CommentBox', () => {
     expect(component.find('button')).to.exist;
   });
 
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
+  });
 });
