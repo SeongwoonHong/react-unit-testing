e14a51334aebb6743c994ae5e2e08f573a31ac5e
seongwoon hong <saytoyou91@gmail.com>
2018-02-20 18:19:41 -0500

    Finished renderComponent helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,6 +2,11 @@ import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
@@ -9,8 +14,15 @@ global.navigator = { userAgent: 'node.js' };
 
 const $ = jquery(global.window);
 
-function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props} />
+    </Provider>
+  );
 
   return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
+
+
+export { renderComponent, expect };
